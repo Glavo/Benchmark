72e86dfa17349d3732f71f95ef28d99093a5987c
Glavo <zjx001202@gmail.com>
2024-12-19 22:19:10 +0800

    update

--- benchmark-1.0/benchmark.sh
+++ benchmark-1.0/benchmark.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
-threadsCount=$(grep -c ^processor /proc/cpuinfo)
+THREADS=$(grep -c ^processor /proc/cpuinfo)
 export CC=gcc-14
 export CXX=g++-14
-
-echo "Threads: $threadsCount"
