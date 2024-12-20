9c1e4889df6a68048da8bbf6c1747704af52b06b
Glavo <zjx001202@gmail.com>
2024-12-20 22:45:11 +0800

    update

--- benchmark-1.0/build-ffmpeg.sh
+++ benchmark-1.0/build-ffmpeg.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 
-FFMPEG_BUILD_DIR="$HOME/ffmpeg_build"
-FFMPEG_BIN_DIR="$FFMPEG_BUILD_DIR/bin"
-FFMPEG_VERSION="7.1"
-
 sudo apt install \
   autoconf automake build-essential cmake git-core libass-dev \
   libfreetype6-dev libgnutls28-dev libmp3lame-dev libsdl2-dev libtool \
   libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev \
   meson ninja-build pkg-config texinfo wget yasm zlib1g-dev \
   libunistring-dev libaom-dev libdav1d-dev libopus-dev
+
+FFMPEG_BUILD_DIR="$HOME/ffmpeg_build"
+FFMPEG_BIN_DIR="$FFMPEG_BUILD_DIR/bin"
+FFMPEG_VERSION="7.1"
 
 cd /tmp && \
 wget -O "ffmpeg-$FFMPEG_VERSION.tar.xz" "https://ffmpeg.org/releases/ffmpeg-$FFMPEG_VERSION.tar.xz" && \
