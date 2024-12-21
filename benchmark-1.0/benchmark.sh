c0f13a18b23d136e7c2c008e5bacd6dec2dfc4b1
Glavo <zjx001202@gmail.com>
2024-12-21 19:14:29 +0800

    update

--- benchmark-1.0/benchmark.sh
+++ benchmark-1.0/benchmark.sh
@@ -3,3 +3,9 @@
 THREADS=$(grep -c ^processor /proc/cpuinfo)
 export CC=gcc-14
 export CXX=g++-14
+
+FFMPEG_BIN_DIR="$HOME/ffmpeg_build/bin"
+
+$FFMPEG_BIN_DIR/ffmpeg -threads $THREADS -c:v hevc -i Bosphorus_h264.mp4 -an -benchmark -f null - 2>&1 | tee h264_decode.txt
+$FFMPEG_BIN_DIR/ffmpeg -threads $THREADS -c:v hevc -i Bosphorus_h265.mp4 -an -benchmark -f null - 2>&1 | tee h265_decode.txt
+
