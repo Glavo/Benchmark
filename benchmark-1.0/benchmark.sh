bc89962bfdb658daafcce02d8933b4bddc965993
Glavo <zjx001202@gmail.com>
2024-12-22 22:34:14 +0800

    update

--- benchmark-1.0/benchmark.sh
+++ benchmark-1.0/benchmark.sh
@@ -9,3 +9,8 @@ FFMPEG_BIN_DIR="$HOME/ffmpeg_build/bin"
 $FFMPEG_BIN_DIR/ffmpeg -threads $THREADS -c:v hevc -i Bosphorus_h264.mp4 -an -benchmark -f null - 2>&1 | tee h264_decode.txt
 $FFMPEG_BIN_DIR/ffmpeg -threads $THREADS -c:v hevc -i Bosphorus_h265.mp4 -an -benchmark -f null - 2>&1 | tee h265_decode.txt
 
+# OpenSSL
+
+opensslCiphers=(md5 sha1 sha256 sha512 des des-ede3 aes-128-cbc aes-192-cbc aes-256-cbc rsa2048 dsa2048)
+openssl speed "${opensslCiphers[@]}" | tee "sslspeed_single.txt"
+openssl speed -multi "$threadsCount" "${opensslCiphers[@]}" | tee "sslspeed_multi.txt"
