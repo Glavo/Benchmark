785ad7a5db74a18208d6154d341eadc6b7c3c8ae
Glavo <zjx001202@gmail.com>
2024-12-25 03:34:12 +0800

    update

--- benchmark-1.0/benchmark.sh
+++ benchmark-1.0/benchmark.sh
@@ -25,7 +25,7 @@ rm /dev/shm/Bosphorus_h265.mp4
 
 ## Encode
 if [ ! -z "$(ffmpeg -encoders 2>/dev/null | grep h264_omx)" ]; then
-   zstd -k -o /tmp/Bosphorus_3840x2160.y4m "$DATA_DIR/Bosphorus_3840x2160.y4m.zst"
+   unzstd -k -o /tmp/Bosphorus_3840x2160.y4m "$DATA_DIR/Bosphorus_3840x2160.y4m.zst"
    ffmpeg -i /tmp/Bosphorus_3840x2160.y4m -c:v h264_omx -an -benchmark -f null - 2>&1 | tee "$RESULT_DIR/h264_encode.txt"
 
    if [ ! -z "$(ffmpeg -encoders 2>/dev/null | grep hevc_omx)" ]; then
@@ -35,8 +35,18 @@ fi
 
 # OpenSSL
 opensslCiphers=(md5 sha1 sha256 sha512 des des-ede3 aes-128-cbc aes-192-cbc aes-256-cbc rsa2048 dsa2048)
+
 openssl speed "${opensslCiphers[@]}" | tee "$RESULT_DIR/openssl_single.txt"
-openssl speed -multi "$threadsCount" "${opensslCiphers[@]}" | tee "$RESULT_DIR/openssl_multi.txt"
+echo -e "\n" >> "$RESULT_DIR/openssl_single.txt"
+openssl speed -evp sm3 | tee -a "$RESULT_DIR/openssl_single.txt"
+echo -e "\n" >> "$RESULT_DIR/openssl_single.txt"
+openssl speed -evp sm4 | tee -a "$RESULT_DIR/openssl_single.txt"
+
+openssl speed -multi $THREADS "${opensslCiphers[@]}" | tee "$RESULT_DIR/openssl_multi.txt"
+echo -e "\n" >> "$RESULT_DIR/openssl_multi.txt"
+openssl speed -multi $THREADS -evp sm3 | tee -a "$RESULT_DIR/openssl_multi.txt"
+echo -e "\n" >> "$RESULT_DIR/openssl_multi.txt"
+openssl speed -multi $THREADS -evp sm4 | tee -a "$RESULT_DIR/openssl_multi.txt"
 
 # 7z (https://www.7-cpu.com/)
 7zz b '-mm=*' '-mmt=*' | tee "$RESULT_DIR/7z-benchmark.txt"
