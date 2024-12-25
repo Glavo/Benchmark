1c9e78eda90797920014deab30a39f288d65222a
Glavo <zjx001202@gmail.com>
2024-12-26 05:22:52 +0800

    update

--- benchmark-1.0/build/build-ffmpeg.sh
+++ benchmark-1.0/build/build-ffmpeg.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/bash
 
-sudo apt install \
-  autoconf automake build-essential cmake git-core libass-dev \
-  libfreetype6-dev libgnutls28-dev libmp3lame-dev libsdl2-dev libtool \
-  libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev \
-  meson ninja-build pkg-config texinfo wget yasm zlib1g-dev \
-  libvpx-dev libx264-dev libx265-dev libnuma-dev \
-  libunistring-dev libaom-dev libdav1d-dev libopus-dev libomxil-bellagio-dev
+
+
+
 
 FFMPEG_BUILD_DIR="$HOME/Build/ffmpeg"
 FFMPEG_BIN_DIR="$FFMPEG_BUILD_DIR/bin"
---
+++ benchmark-1.0/init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo apt install \
+    fish sensors curl wget zstd lz4 xz-utils 7zip \
+    autoconf automake build-essential cmake git g++ ninja-build \
+    libass-dev libfreetype-dev libgnutls28-dev libmp3lame-dev libsdl2-dev libtool \
+    libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev \
+    meson ninja-build pkg-config texinfo wget yasm zlib1g-dev \
+    libvpx-dev libx264-dev libx265-dev libnuma-dev \
+    libunistring-dev libaom-dev libdav1d-dev libopus-dev libomxil-bellagio-dev
