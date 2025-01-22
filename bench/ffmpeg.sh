ccf5bad8468c8dec81503ea333b59ce2c0ed8775
Glavo <zjx001202@gmail.com>
2025-01-22 19:37:04 +0800

    update

--- bench/ffmpeg.sh
+++ bench/ffmpeg.sh
@@ -4,8 +4,8 @@ set -e
 
 ## Decode
 cp $DATA_DIR/Bosphorus_*.mp4 /dev/shm
-ffmpeg -threads $THREADS -c:v h264 -i /dev/shm/Bosphorus_h264.mp4 -an -benchmark -f null - 2>&1 | tee "$RESULT_DIR/h264_decode.txt"
-ffmpeg -threads $THREADS -c:v hevc -i /dev/shm/Bosphorus_h265.mp4 -an -benchmark -f null - 2>&1 | tee "$RESULT_DIR/h265_decode.txt"
+ffmpeg -threads "$THREADS" -c:v h264 -i /dev/shm/Bosphorus_h264.mp4 -an -benchmark -f null - 2>&1 | tee "$RESULT_DIR/h264_decode.txt"
+ffmpeg -threads "$THREADS" -c:v hevc -i /dev/shm/Bosphorus_h265.mp4 -an -benchmark -f null - 2>&1 | tee "$RESULT_DIR/h265_decode.txt"
 rm /dev/shm/Bosphorus_*.mp4
 
 set +e
@@ -22,5 +22,5 @@ else
     exit
 fi
 
-ffmpeg -i "$DATA_DIR/Bosphorus_3840x2160.y4m" -c:v $H264_ENCODER -an -benchmark -f null - 2>&1 | tee "$RESULT_DIR/h264_encode.txt"
-ffmpeg -i "$DATA_DIR/Bosphorus_3840x2160.y4m" -c:v $H265_ENCODER -an -benchmark -f null - 2>&1 | tee "$RESULT_DIR/h265_encode.txt"
+ffmpeg -i "$DATA_DIR/Bosphorus_3840x2160.y4m" -c:v "$H264_ENCODER" -an -benchmark -f null - 2>&1 | tee "$RESULT_DIR/h264_encode.txt"
+ffmpeg -i "$DATA_DIR/Bosphorus_3840x2160.y4m" -c:v "$H265_ENCODER" -an -benchmark -f null - 2>&1 | tee "$RESULT_DIR/h265_encode.txt"
