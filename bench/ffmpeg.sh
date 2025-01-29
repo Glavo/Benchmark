bfdef5dadd65e943d30e22d128c854eed043de7b
Glavo <zjx001202@gmail.com>
2025-01-29 17:43:14 +0800

    update

--- bench/ffmpeg.sh
+++ bench/ffmpeg.sh
@@ -4,20 +4,22 @@ set -e
 
 ## Decode
 cp "$DATA_DIR"/Bosphorus_*.mp4 /dev/shm
-ffmpeg -threads "$THREADS" -c:v h264 -i /dev/shm/Bosphorus_h264.mp4 -an -benchmark -f null - 2>&1 | tee "$RESULT_DIR/h264_decode.txt"
-ffmpeg -threads "$THREADS" -c:v hevc -i /dev/shm/Bosphorus_h265.mp4 -an -benchmark -f null - 2>&1 | tee "$RESULT_DIR/h265_decode.txt"
-rm /dev/shm/Bosphorus_*.mp4
+ffmpeg -threads "$THREADS" -c:v h264 -i /dev/shm/Bosphorus_h264.mp4 -an -benchmark -f null - 2>&1 | tee "$RESULT_DIR/h264_software_decode.txt"
+ffmpeg -threads "$THREADS" -c:v hevc -i /dev/shm/Bosphorus_h265.mp4 -an -benchmark -f null - 2>&1 | tee "$RESULT_DIR/h265_software_decode.txt"
 
 set +e
 
-## Encode
-
-if [ "$(uname -m)" = "riscv64" ]; then
-    H264_ENCODER='h264_omx'
-    H265_ENCODER='hevc_omx'
-else
-    exit
+if [ -z "$H264_HARDWARE_DECODER" ]; then
+  ffmpeg -c:v "$H264_HARDWARE_DECODER" -i /dev/shm/Bosphorus_h264.mp4 -an -benchmark -f null - 2>&1 | tee "$RESULT_DIR/h264_hardware_decode.txt"
 fi
 
-ffmpeg -i "$DATA_DIR/Bosphorus_3840x2160.y4m" -c:v "$H264_ENCODER" -an -benchmark -pix_fmt yuv420p -f null - 2>&1 | tee "$RESULT_DIR/h264_encode.txt"
-ffmpeg -i "$DATA_DIR/Bosphorus_3840x2160.y4m" -c:v "$H265_ENCODER" -an -benchmark -pix_fmt yuv420p -f null - 2>&1 | tee "$RESULT_DIR/h265_encode.txt"
+if [ -z "$H265_HARDWARE_DECODER" ]; then
+  ffmpeg -c:v "$H265_HARDWARE_DECODER" -i /dev/shm/Bosphorus_h265.mp4 -an -benchmark -f null - 2>&1 | tee "$RESULT_DIR/h265_hardware_decode.txt"
+fi
+
+rm /dev/shm/Bosphorus_*.mp4
+
+## Encode
+
+#ffmpeg -i "/dev/shm/Bosphorus_3840x2160.y4m" -c:v "$H264_HARDWARE_ENCODER" -an -benchmark -pix_fmt yuv420p -f null - 2>&1 | tee "$RESULT_DIR/h264_hardware_encode.txt"
+#ffmpeg -i "/dev/shm/Bosphorus_3840x2160.y4m" -c:v "$H265_HARDWARE_ENCODER" -an -benchmark -pix_fmt yuv420p -f null - 2>&1 | tee "$RESULT_DIR/h265_hardware_encode.txt"
