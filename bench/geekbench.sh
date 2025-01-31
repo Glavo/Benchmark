811a42e38d30f00991b1894ac54d53073342f8f5
Glavo <zjx001202@gmail.com>
2025-01-31 13:35:00 +0800

    update

--- bench/geekbench.sh
+++ bench/geekbench.sh
@@ -19,14 +19,14 @@ _GB_CDN="https://cdn.geekbench.com"
 _GB5_NAME="Geekbench-5.5.1-$_GEEKBENCH_SUFFIX"
 _GB6_NAME="Geekbench-6.4.0-$_GEEKBENCH_SUFFIX"
 
-if [ ! -d "$CACHE_DIR/$_GB5_NAME" ]; then
-    if [ ! -f "$CACHE_DIR/$_GB5_NAME.tar.gz" ]; then
-        wget -O "$CACHE_DIR/$_GB5_NAME.tar.gz.tmp" "$_GB_CDN/$_GB5_NAME.tar.gz"
-        mv "$CACHE_DIR/$_GB5_NAME.tar.gz.tmp" "$CACHE_DIR/$_GB5_NAME.tar.gz"
-    fi
-    tar -xvf "$CACHE_DIR/$_GB5_NAME.tar.gz" -C "$CACHE_DIR"
-    rm "$CACHE_DIR/$_GB5_NAME.tar.gz"
-fi
+#if [ ! -d "$CACHE_DIR/$_GB5_NAME" ]; then
+#    if [ ! -f "$CACHE_DIR/$_GB5_NAME.tar.gz" ]; then
+#        wget -O "$CACHE_DIR/$_GB5_NAME.tar.gz.tmp" "$_GB_CDN/$_GB5_NAME.tar.gz"
+#        mv "$CACHE_DIR/$_GB5_NAME.tar.gz.tmp" "$CACHE_DIR/$_GB5_NAME.tar.gz"
+#    fi
+#    tar -xvf "$CACHE_DIR/$_GB5_NAME.tar.gz" -C "$CACHE_DIR"
+#    rm "$CACHE_DIR/$_GB5_NAME.tar.gz"
+#fi
 
 if [ ! -d "$CACHE_DIR/$_GB6_NAME" ]; then
     if [ ! -f "$CACHE_DIR/$_GB6_NAME.tar.gz" ]; then
@@ -37,5 +37,5 @@ if [ ! -d "$CACHE_DIR/$_GB6_NAME" ]; then
     rm "$CACHE_DIR/$_GB6_NAME.tar.gz"
 fi
 
-"$CACHE_DIR/$_GB5_NAME/geekbench5" | tee "$RESULT_DIR/geekbench5.log"
+#"$CACHE_DIR/$_GB5_NAME/geekbench5" | tee "$RESULT_DIR/geekbench5.log"
 "$CACHE_DIR/$_GB6_NAME/geekbench6" | tee "$RESULT_DIR/geekbench6.log"
